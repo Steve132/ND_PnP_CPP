d4cc8fe318553fc2ba609ad802834c6d07339953
Steve132 <steve@soapforge.com>
2020-05-07 00:16:45 -0400

    Fixed broken initialization bug

--- PnPDynamic.cpp
+++ PnPDynamic.cpp
@@ -67,11 +67,12 @@ Eigen::MatrixXd PnP_dynamic_in_place(Eigen::MatrixXd& Y,Eigen::MatrixXd& X,bool 
 		{
 			throw std::runtime_error("Not enough sample points for this dimensionality of problem");
 		}
-		Eigen::MatrixXd A(l*N+(allow_last_element_zero ? 0 : 1),L*M);
-		Eigen::MatrixXd Ablock(l,L*M);
+		Eigen::MatrixXd A=Eigen::MatrixXd::Zero(l*N+(allow_last_element_zero ? 0 : 1),L*M);
+		Eigen::MatrixXd Ablock;
 		
 		for(size_t i=0;i<N;i++)
 		{
+			Ablock=Eigen::MatrixXd::Zero(l,L*M);
 			Eigen::RowVectorXd xT=X.col(i).transpose();
 			Eigen::VectorXd y=Y.col(i);
 			Ablock.rightCols(M)=-y.topRows(l)*xT; //outer product for homogenous section of block
@@ -81,6 +82,7 @@ Eigen::MatrixXd PnP_dynamic_in_place(Eigen::MatrixXd& Y,Eigen::MatrixXd& X,bool 
 			}
 			A.block(i*l,0,l,L*M)=Ablock;
 		}
+		std::cout << A << std::endl;
 		
 		//std::cout << "A:\n" << A << std::endl;
 		//Y=FX
@@ -121,11 +123,12 @@ Eigen::MatrixXd PnP_dynamic_in_place(Eigen::MatrixXd& Y,Eigen::MatrixXd& X,bool 
 		Fp.transposeInPlace();
 
 	}
-	std::cout << "Fp:\n" << Fp << std::endl;
+	//std::cout << "Fp:\n" << Fp << std::endl;
 	//F'Px X=Py Y
 	//inv(Py) F' Px X=Y
 	//(inv(Py) F' Px) X=Y
 	//F=inv(Py) F' Px
-	return Py.inverse()*Fp*Px;
+	Eigen::MatrixXd result=(Py.inverse()*Fp)*Px;
+	return result;
 }
 
